008616c5c016e653947f1ddb3a275947f78d1e15
Matt Mead <matt.a.mead@gmail.com>
2019-08-07 08:47:45 -0700

    Working

--- main.cpp
+++ main.cpp
@@ -18,20 +18,23 @@ int main()
 		httpResponseMessage.EnsureSuccessStatusCode();
 		httpResponseBody = httpResponseMessage.Content().ReadAsStringAsync().get();
 
-		JsonObject jsonObject = JsonObject();
-		bool canParse = JsonObject::TryParse(httpResponseBody, jsonObject);
-		bool hasDataKey = jsonObject.HasKey(L"data"); // Debug, look for "data" key at the beginning
-		IJsonValue dataValue = jsonObject.Lookup(L"data");
-		hstring s = dataValue.Stringify();
-
-		JsonObject dataObject = JsonObject();
-		bool canParseAgain = JsonObject::TryParse(s, dataObject);
-		bool hasAmountKey = dataObject.HasKey(L"amount"); // Debug
-		hstring amountValue = dataObject.GetNamedString(L"amount");
 	}
 	catch (hresult_error const& ex)
 	{
 		httpResponseBody = ex.message();
 	}
-	std::wcout << httpResponseBody;
+
+	JsonObject jsonObject = JsonObject();
+	bool canParse = JsonObject::TryParse(httpResponseBody, jsonObject);
+	bool hasDataKey = jsonObject.HasKey(L"data"); // Debug, look for "data" key at the beginning
+	IJsonValue dataValue = jsonObject.Lookup(L"data");
+	hstring s = dataValue.Stringify();
+
+	JsonObject dataObject = JsonObject();
+	bool canParseAgain = JsonObject::TryParse(s, dataObject);
+	bool hasAmountKey = dataObject.HasKey(L"amount"); // Debug
+	hstring amountValue = dataObject.GetNamedString(L"amount");
+
+	//std::wcout << httpResponseBody.c_str() << std::endl;
+	std::wcout << "$" << amountValue.c_str();
 }
