96e437007cf78ee155f40c5bb2f2049e0bf2c66e
Matt Mead <matt.a.mead@gmail.com>
2019-08-07 08:20:49 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web::Http;
+using namespace Windows::Data::Json;
+
+int main()
+{
+    init_apartment();
+	HttpClient httpClient;
+	Uri requestUri{ L"https://api.coinbase.com/v2/prices/spot?currency=USD" };
+	HttpResponseMessage httpResponseMessage;
+	hstring httpResponseBody;
+	try
+	{
+		httpResponseMessage = httpClient.GetAsync(requestUri).get();
+		httpResponseMessage.EnsureSuccessStatusCode();
+		httpResponseBody = httpResponseMessage.Content().ReadAsStringAsync().get();
+
+		JsonObject jsonObject = JsonObject();
+		bool canParse = JsonObject::TryParse(httpResponseBody, jsonObject);
+		bool hasDataKey = jsonObject.HasKey(L"data"); // Debug, look for "data" key at the beginning
+		IJsonValue dataValue = jsonObject.Lookup(L"data");
+		hstring s = dataValue.Stringify();
+
+		JsonObject dataObject = JsonObject();
+		bool canParseAgain = JsonObject::TryParse(s, dataObject);
+		bool hasAmountKey = dataObject.HasKey(L"amount"); // Debug
+		hstring amountValue = dataObject.GetNamedString(L"amount");
+	}
+	catch (hresult_error const& ex)
+	{
+		httpResponseBody = ex.message();
+	}
+	std::wcout << httpResponseBody;
+}
---
+++ pch.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include "winrt/Windows.Web.Http.h"
+#include <iostream>
+#include <winrt/Windows.Data.Json.h>
